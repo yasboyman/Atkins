6a491e0bf52f80d924e69c036923905d714c6f5b
yasir <yasiralabadi@hotmail.com>
2021-10-25 21:24:35 +0100

    Titles and subtitles css complete, buttons added, no css yet

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -12,28 +12,40 @@ const HomePage = () => {
         alt={"background"}
         style={{ backgroundSize: "cover", zIndex: "-1" }}
       />
+      <nav
+        className={"nav"}
+        style={{ color: "white", fontSize: "20px", marginTop: "2%" }}
+      >
+        <div>
+          <img
+            src={companyLogo}
+            alt={"company logo"}
+            style={{ marginRight: "260px", marginLeft: "120px" }}
+          />
+        </div>
+        <NavigationItems item={"About Us"} />
+        <NavigationItems item={"Partners"} />
+        <NavigationItems item={"Strategy"} />
+        <NavigationItems item={"Our Work"} />
+        <NavigationItems item={"News"} />
+        <NavigationItems item={"WPVC"} />
+        <NavigationItems item={"Videos"} />
+        <NavigationItems item={"Contact Us"} />
+      </nav>
 
-      <div className={"top_navigation_container"}>
-        <nav
-          className={"nav"}
-          style={{ color: "white", fontSize: "20px", marginTop: "2%" }}
-        >
-          <div>
-            <img
-              src={companyLogo}
-              alt={"company logo"}
-              style={{ marginRight: "260px", marginLeft: "120px" }}
-            />
-          </div>
-          <NavigationItems item={"About Us"} />
-          <NavigationItems item={"Partners"} />
-          <NavigationItems item={"Strategy"} />
-          <NavigationItems item={"Our Work"} />
-          <NavigationItems item={"News"} />
-          <NavigationItems item={"WPVC"} />
-          <NavigationItems item={"Videos"} />
-          <NavigationItems item={"Contact Us"} />
-        </nav>
+      <div className={"Middle_Text_Container"}>
+        <h1 className={"title"}>
+          We're <span className={"title_with_span"}> transforming</span> the way
+          you work.
+        </h1>
+        <p className={"subtitle"}>
+          Manage your schedule with our cloud based software
+        </p>
+
+        <div className={"buttons_container"}>
+          <button className={"signUp_btn"}> Sign Up</button>
+          <button className={"learnMore_btn"}> Learn More</button>
+        </div>
       </div>
     </div>
   );
